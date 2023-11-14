1bf63b1f973e202ae01193a65231f9e572ac61be
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-14 02:09:56 +0000

    upload > function `createCard`

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 import { copiarTexto } from './copy.js';
 
+const mainEl = document.querySelector('main');
 const backgroundCard = document.querySelectorAll('.card');
 const animationIcon = document.querySelector('.title-header i');
+
+// function createCard() {
+//   for (let i = 0; i <= 30; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('card');
+//     mainEl.appendChild(div);
+//   }
+// }
+// createCard();
 
 animationIcon.addEventListener('click', () => {
   animationIcon.classList.add('animation-icon');
